8c9e88144e0ca4472a62ab3edcc03eb81329be72
Aaron Sprouse <115891926+absprouse23@users.noreply.github.com>
2024-07-16 14:04:40 +0000

    move compiler and setup rule saves

--- src/main.c
+++ src/main.c
@@ -1,7 +1,4 @@
-#include <dirent.h>
-#include <unistd.h>
-#include <yara.h>
-#include <yara/compiler.h>
+#include "compiler.h"
 
 int main(int argc, char** argv) {
 
@@ -10,41 +7,14 @@ int main(int argc, char** argv) {
         return EXIT_FAILURE;
     }
 
-    YR_COMPILER* compiler;
+    YR_RULES* rules;
 
-    if (yr_compiler_create(&compiler) != ERROR_SUCCESS) {
-        fprintf(stderr, "Failed to create YARA compiler\n");
+    if (compiler_build_ruleset(&rules) == EXIT_FAILURE) {
         yr_finalize();
         return EXIT_FAILURE;
     }
 
-    DIR* dir_stream = opendir("./rules/");
-    if (dir_stream == NULL) {
-        perror("Cannot open rules directory. Does it exist?");
-        yr_compiler_destroy(compiler);
-        yr_finalize();
-        return EXIT_FAILURE;
-    }
+    yr_rules_save(rules, "rules.dat");
 
-    struct dirent *dir;
-    while ((dir = readdir(dir_stream)) != NULL) {
-        if ((strcmp(dir->d_name, ".") == 0) || (strcmp(dir->d_name, "..") == 0)) {
-            continue;
-        }
-        printf("Compiling rule %s\n", dir->d_name);
-
-        chdir("rules");
-        FILE* current_file = fopen(dir->d_name, "r");
-        int error = yr_compiler_add_file(compiler, current_file , NULL, NULL);
-
-        if (error > 0) {
-            fprintf(stderr, "Failed to compile rule %s\n", dir->d_name);
-            compiler->errors = 0;
-        }
-        fclose(current_file);
-        chdir("..");
-    }
-
-    yr_compiler_destroy(compiler);
     yr_finalize();
 }
